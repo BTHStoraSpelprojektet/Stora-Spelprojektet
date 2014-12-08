bb2a383b1cf33ceeaa5e573b07b268549274c41e
Pappskiva <alleo92@hotmail.com>
2014-12-08 11:31:37 +0100

    b working like a bee

--- Shurikenjutsu/Shurikenjutsu/PlayerManager.cpp
+++ Shurikenjutsu/Shurikenjutsu/PlayerManager.cpp
@@ -14,7 +14,7 @@ PlayerManager::~PlayerManager()
 bool PlayerManager::Initialize(std::vector<Object> p_ModelList)
 {
 	m_enemyList = std::vector<Player>();
-	AddPlayer("../Shurikenjutsu/Models/cubemanWnP.SSP", DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f), DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f), 10.0f, 100, 5, 100, 20);
+	AddPlayer("../Shurikenjutsu/Models/cubemanWnP.SSP", DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f), DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f), 10.0f, 100, 5, 100, 20, p_ModelList);
 
 	return true;
 }
