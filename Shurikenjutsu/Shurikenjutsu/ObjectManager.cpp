0f0028c6a2362572ac4aa2f6674bf22964cd86b5
Johan Almeflo <johan_almeflo@hotmail.com>
2014-12-09 08:33:07 +0100

    Comments removed

--- Shurikenjutsu/Shurikenjutsu/ObjectManager.cpp
+++ Shurikenjutsu/Shurikenjutsu/ObjectManager.cpp
@@ -29,11 +29,6 @@ void ObjectManager::Update(double p_deltaTime)
 	for (unsigned int i = 0; i < m_shurikens.size(); i++)
 	{
 		m_shurikens[i].Update(p_deltaTime);
-		//if (m_shurikens[i].IsDead())
-		//{
-		//	m_shurikens.erase(m_shurikens.begin()+i);
-		//	i--;
-		//}
 	}
 
 	if (Network::IsShurikenListUpdated())
