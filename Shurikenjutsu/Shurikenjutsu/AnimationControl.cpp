552a3d78d88f992f4f1ea073ca86afbcd2437fcc
Michael <jansson57@msn.com>
2014-12-16 11:00:47 +0100

    Updated Animation Controller

--- Shurikenjutsu/Shurikenjutsu/AnimationControl.cpp
+++ Shurikenjutsu/Shurikenjutsu/AnimationControl.cpp
@@ -25,17 +25,17 @@ std::vector<DirectX::XMFLOAT4X4> AnimationControl::UpdateAnimation()
 	m_frameArms += deltaTime * 20;
 	m_frameLegs += deltaTime * 20;
 
-	if (m_frameArms >= (m_animationStacks[2].m_endFrame - 1))
+	if (m_frameArms >= (m_animationStacks[9].m_endFrame - 1))
 		m_frameArms = 0.0f;
 
-	if (m_frameLegs >= (m_animationStacks[0].m_endFrame - 1))
+	if (m_frameLegs >= (m_animationStacks[10].m_endFrame - 1))
 		m_frameLegs = 0.0f;
 
 	DirectX::XMVECTOR startQuaternion = DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 1.0f);
 	DirectX::XMVECTOR startTranslation = DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 1.0f);
 
 	int* index = new int(0);
-	CombineMatrices(index, m_animationStacks[2].m_root[(int)m_frameArms], m_animationStacks[0].m_root[(int)m_frameLegs], startQuaternion, startTranslation);
+	CombineMatrices(index, m_animationStacks[9].m_root[(int)m_frameArms], m_animationStacks[10].m_root[(int)m_frameLegs], startQuaternion, startTranslation);
 	delete[] index;
 
 	return m_boneTransforms;
@@ -43,9 +43,9 @@ std::vector<DirectX::XMFLOAT4X4> AnimationControl::UpdateAnimation()
 
 void AnimationControl::CombineMatrices(int* p_index, BoneFrame* p_jointArms, BoneFrame* p_jointLegs, DirectX::XMVECTOR& p_parentQuaternion, DirectX::XMVECTOR& p_parentTranslation)
 {
-	DirectX::XMVECTOR quaternion = DirectX::XMVectorSet(p_jointArms->m_quaternion[0], p_jointArms->m_quaternion[1], p_jointArms->m_quaternion[2], p_jointArms->m_quaternion[3]);
-	//DirectX::XMVECTOR quaternionLegs = DirectX::XMVectorSet(p_jointLegs->m_quaternion[0], p_jointLegs->m_quaternion[1], p_jointLegs->m_quaternion[2], p_jointLegs->m_quaternion[3]);
-	//DirectX::XMVECTOR quaternion = DirectX::XMQuaternionMultiply(quaternionArms, quaternionLegs);
+	DirectX::XMVECTOR quaternionArms = DirectX::XMVectorSet(p_jointArms->m_quaternion[0], p_jointArms->m_quaternion[1], p_jointArms->m_quaternion[2], p_jointArms->m_quaternion[3]);
+	DirectX::XMVECTOR quaternionLegs = DirectX::XMVectorSet(p_jointLegs->m_quaternion[0], p_jointLegs->m_quaternion[1], p_jointLegs->m_quaternion[2], p_jointLegs->m_quaternion[3]);
+	DirectX::XMVECTOR quaternion = DirectX::XMQuaternionMultiply(quaternionArms, quaternionLegs);
 	DirectX::XMVECTOR orientQuaternion = DirectX::XMVectorSet(p_jointArms->m_orientQuaternion[0], p_jointArms->m_orientQuaternion[1], p_jointArms->m_orientQuaternion[2], p_jointArms->m_orientQuaternion[3]);
 
 	quaternion = DirectX::XMQuaternionMultiply(quaternion, orientQuaternion);
@@ -65,9 +65,9 @@ void AnimationControl::CombineMatrices(int* p_index, BoneFrame* p_jointArms, Bon
 	parentMatrix.r[3].m128_f32[2] = p_parentTranslation.m128_f32[2];
 
 	float combinedTranslation[3];
-	combinedTranslation[0] = (p_jointArms->m_translation[0]);// + p_jointLegs->m_translation[0]) / 2;
-	combinedTranslation[1] = (p_jointArms->m_translation[1]);// + p_jointLegs->m_translation[1]) / 2;
-	combinedTranslation[2] = (p_jointArms->m_translation[2]);// + p_jointLegs->m_translation[2]) / 2;
+	combinedTranslation[0] = (p_jointArms->m_translation[0] + p_jointLegs->m_translation[0]) / 2;
+	combinedTranslation[1] = (p_jointArms->m_translation[1] + p_jointLegs->m_translation[1]) / 2;
+	combinedTranslation[2] = (p_jointArms->m_translation[2] + p_jointLegs->m_translation[2]) / 2;
 
 	DirectX::XMVECTOR jointTranslation = DirectX::XMVectorSet(combinedTranslation[0], combinedTranslation[1], combinedTranslation[2], 1.0f);
 
