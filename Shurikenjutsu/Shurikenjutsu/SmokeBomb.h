36e02c5ebf38ba9de76ba6a516f2f02cb3228989
Pappskiva <alleo92@hotmail.com>
2014-12-17 16:19:13 +0100

    added filesa lol

---
+++ Shurikenjutsu/Shurikenjutsu/SmokeBomb.h
@@ -0,0 +1,8 @@
+#pragma once
+class SmokeBomb
+{
+public:
+	SmokeBomb();
+	~SmokeBomb();
+};
+
---
+++ Shurikenjutsu/Shurikenjutsu/SmokeBombAbility.cpp
@@ -0,0 +1,11 @@
+#include "SmokeBombAbility.h"
+
+
+SmokeBombAbility::SmokeBombAbility()
+{
+}
+
+
+SmokeBombAbility::~SmokeBombAbility()
+{
+}
