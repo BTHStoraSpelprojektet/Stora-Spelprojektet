a3fc07b2e4a4bc6d94110f62137ae06d381daa9a
Lunkn <Phelsuma_66@hotmail.com>
2015-01-19 09:41:25 +0100

    quadtree class

---
+++ Shurikenjutsu/Shurikenjutsu/QuadTree.cpp
@@ -0,0 +1,16 @@
+#include "QuadTree.h"
+
+QuadTree::QuadTree()
+{
+	m_vertexList = 0;
+	m_parentNode = 0;
+}
+
+QuadTree::QuadTree(const QuadTree& other){}
+
+QuadTree::~QuadTree(){}
+
+bool QuadTree::Initialize()
+{
+
+}
