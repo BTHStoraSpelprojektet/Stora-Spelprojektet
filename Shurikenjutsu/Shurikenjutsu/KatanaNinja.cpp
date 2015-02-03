96d8e1aba41673df117b10de4879f737a7cb9743
Johan Almeflo <johan_almeflo@hotmail.com>
2015-02-03 11:41:01 +0100

    heap size fix for ninjas on new command

--- Shurikenjutsu/Shurikenjutsu/KatanaNinja.cpp
+++ Shurikenjutsu/Shurikenjutsu/KatanaNinja.cpp
@@ -10,6 +10,16 @@
 KatanaNinja::KatanaNinja(){}
 KatanaNinja::~KatanaNinja(){}
 
+void* KatanaNinja::operator new(size_t p_i)
+{
+	return _mm_malloc(p_i, 16);
+}
+
+void KatanaNinja::operator delete(void* p_p)
+{
+	_mm_free(p_p);
+}
+
 bool KatanaNinja::Initialize(DirectX::XMFLOAT3 p_pos, DirectX::XMFLOAT3 p_direction)
 {
 	bool result;
