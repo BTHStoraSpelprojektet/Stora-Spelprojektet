8e4b33c1dd0b1b865cc214e7caed0f755b2137b1
Johan Almeflo <johan_almeflo@hotmail.com>
2015-02-27 10:25:33 +0100

    Debug text added :candy:
    Press F1 to turn it off

--- Shurikenjutsu/Shurikenjutsu/MemoryChecker.cpp
+++ Shurikenjutsu/Shurikenjutsu/MemoryChecker.cpp
@@ -3,6 +3,7 @@
 #include <Psapi.h>
 #include <iostream>
 #include <string>
+#include "DebugText.h"
 
 MemoryChecker::MemoryChecker(){}
 MemoryChecker::~MemoryChecker(){}
@@ -21,18 +22,23 @@ void MemoryChecker::Shutdown()
 void MemoryChecker::Update()
 {
 	PROCESS_MEMORY_COUNTERS_EX pmc;
-	bool test = GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
+	GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
 	SIZE_T virtualMemUsedByMe = pmc.PrivateUsage;
 
-	SIZE_T vkbmu = (virtualMemUsedByMe / (SIZE_T)1024.0);
-	SIZE_T vmbmu = (vkbmu / (SIZE_T)1024.0);
+	// Check virtual ram used
+	SIZE_T vkbmu = (virtualMemUsedByMe / (SIZE_T)1024.0); // virtual KB memory used
+	SIZE_T vmbmu = (vkbmu / (SIZE_T)1024.0); // virtual MB memory used
 	m_virtualMBUsed = (int)vmbmu;
 
-
+	// Check physical ram used
 	SIZE_T physMemUsedByMe = pmc.WorkingSetSize;
-	SIZE_T pkbmu = (physMemUsedByMe / (SIZE_T)1024.0);
-	SIZE_T pmbmu = (pkbmu / (SIZE_T)1024.0);
+	SIZE_T pkbmu = (physMemUsedByMe / (SIZE_T)1024.0); // physical KB memory used
+	SIZE_T pmbmu = (pkbmu / (SIZE_T)1024.0); // physical MB memory used
 	m_physicalMBUsed = (int)pmbmu;
+
+	// Set values to debugText
+	DebugText::GetInstance()->SetVRAMText(m_virtualMBUsed);
+	DebugText::GetInstance()->SetRAMText(m_physicalMBUsed);
 }
 
 int MemoryChecker::GetVitrualMBUsed()
