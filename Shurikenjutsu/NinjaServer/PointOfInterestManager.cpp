b18e882ca9314690d4159dd780736213c5b5a10e
Lunkn <Phelsuma_66@hotmail.com>
2015-03-02 16:48:33 +0100

    commit

--- Shurikenjutsu/NinjaServer/PointOfInterestManager.cpp
+++ Shurikenjutsu/NinjaServer/PointOfInterestManager.cpp
@@ -29,9 +29,29 @@ bool PointOfInterestManager::Initialize(RakNet::RakPeerInterface *p_serverPeer)
 	m_nrOfRunes = 3;
 
 	m_lotusBoundingBoxes = ModelLibrary::GetInstance()->GetModel(RUNE_LOTUS)->GetBoundingBoxes();
-	m_shieldBoundingBoxes = ModelLibrary::GetInstance()->GetModel(RUNE_SHIELD)->GetBoundingBoxes();
-	m_invisBoundingBoxes = ModelLibrary::GetInstance()->GetModel(RUNE_INVIS)->GetBoundingBoxes();
+	for (unsigned int i = 0; i < m_lotusBoundingBoxes.size(); i++)
+	{
+		m_lotusBoundingBoxes[i].m_center.x += m_POISpawnPoints[0].m_translationX;
+		m_lotusBoundingBoxes[i].m_center.y += m_POISpawnPoints[0].m_translationY;
+		m_lotusBoundingBoxes[i].m_center.z += m_POISpawnPoints[0].m_translationZ;
+	}
 
+	m_invisBoundingBoxes = ModelLibrary::GetInstance()->GetModel(RUNE_INVIS)->GetBoundingBoxes();
+	for (unsigned int i = 0; i < m_invisBoundingBoxes.size(); i++)
+	{
+		m_invisBoundingBoxes[i].m_center.x += m_POISpawnPoints[1].m_translationX;
+		m_invisBoundingBoxes[i].m_center.y += m_POISpawnPoints[1].m_translationY;
+		m_invisBoundingBoxes[i].m_center.z += m_POISpawnPoints[1].m_translationZ;
+	}
+	
+	m_shieldBoundingBoxes = ModelLibrary::GetInstance()->GetModel(RUNE_SHIELD)->GetBoundingBoxes();
+	for (unsigned int i = 0; i < m_shieldBoundingBoxes.size(); i++)
+	{
+		m_shieldBoundingBoxes[i].m_center.x += m_POISpawnPoints[2].m_translationX;
+		m_shieldBoundingBoxes[i].m_center.y += m_POISpawnPoints[2].m_translationY;
+		m_shieldBoundingBoxes[i].m_center.z += m_POISpawnPoints[2].m_translationZ;
+	}
+	
 	return true;
 }
 
