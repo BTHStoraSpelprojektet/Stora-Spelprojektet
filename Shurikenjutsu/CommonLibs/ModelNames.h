4a4d91331939cec5384cb5b8e8943f307ff4e71f
Jonas Axelsson <axelsson.92@live.se>
2015-03-06 11:53:59 +0100

    Moved every texture.

--- Shurikenjutsu/CommonLibs/ModelNames.h
+++ Shurikenjutsu/CommonLibs/ModelNames.h
@@ -25,75 +25,73 @@
 #define FANBOOMERANG_MODEL_NAME "../Shurikenjutsu/Models/TessenShape.SSP"
 
 // Minimap
-#define MINIMAP_TEXTURE "../Shurikenjutsu/2DTextures/minimap.png"
-#define MINIMAP_RED_DOT_TEXTURE "../Shurikenjutsu/2DTextures/minimap_redDot.png"
-#define MINIMAP_BLUE_DOT_TEXTURE "../Shurikenjutsu/2DTextures/minimap_blueDot.png"
-#define MINIMAP_BG_TEXTURE "../Shurikenjutsu/2DTextures/minimapframe.png"
+#define MINIMAP_TEXTURE "../Shurikenjutsu/2DTextures/GUI/minimap.png"
+#define MINIMAP_RED_DOT_TEXTURE "../Shurikenjutsu/2DTextures/GUI/minimap_redDot.png"
+#define MINIMAP_BLUE_DOT_TEXTURE "../Shurikenjutsu/2DTextures/GUI/minimap_blueDot.png"
+#define MINIMAP_BG_TEXTURE "../Shurikenjutsu/2DTextures/GUI/minimapframe.png"
 
 // Abilities
-#define ABILITY_BG_TEXTURE "../Shurikenjutsu/2DTextures/tb_bg.png"
-#define ABILITY_CD_TEXTURE "../Shurikenjutsu/2DTextures/TB_cd.png"
+#define ABILITY_BG_TEXTURE "../Shurikenjutsu/2DTextures/GUI/tb_bg.png"
+#define ABILITY_CD_TEXTURE "../Shurikenjutsu/2DTextures/GUI/TB_cd.png"
 
-#define ABILITY_KATANA_TEXTURE "../Shurikenjutsu/2DTextures/TB_N1_Melee.png"
-#define ABILITY_SHURIKEN_TEXTURE "../Shurikenjutsu/2DTextures/TB_N1_Ranged.png"
-#define ABILITY_MEGA_SHURIKEN_TEXTURE "../Shurikenjutsu/2DTextures/TB_N1_E.png"
-#define ABILITY_DASH_TEXTURE "../Shurikenjutsu/2DTextures/TB_N1_Q.png"
-#define ABILITY_SMOKE_BOMB_TEXTURE "../Shurikenjutsu/2DTextures/TB_SmokeBomb.png"
+#define ABILITY_KATANA_TEXTURE "../Shurikenjutsu/2DTextures/Abilities/TB_N1_Melee.png"
+#define ABILITY_SHURIKEN_TEXTURE "../Shurikenjutsu/2DTextures/Abilities/TB_N1_Ranged.png"
+#define ABILITY_MEGA_SHURIKEN_TEXTURE "../Shurikenjutsu/2DTextures/Abilities/TB_N1_E.png"
+#define ABILITY_DASH_TEXTURE "../Shurikenjutsu/2DTextures/Abilities/TB_N1_Q.png"
+#define ABILITY_SMOKE_BOMB_TEXTURE "../Shurikenjutsu/2DTextures/Abilities/TB_SmokeBomb.png"
 
-#define ABILITY_TESSEN_TEXTURE "../Shurikenjutsu/2DTextures/TB_N2_Melee.png"
-#define ABILITY_TESSEN_THROWING_TEXTURE "../Shurikenjutsu/2DTextures/TB_N2_Q.png"
-#define ABILITY_WHIP_TEXTURE "../Shurikenjutsu/2DTextures/TB_N2_Ranged.png"
-#define ABILITY_WHIP_SPECIAL_TEXTURE "../Shurikenjutsu/2DTextures/TB_N2_E.png"
-#define ABILITY_CALTROP_TEXTURE "../Shurikenjutsu/2DTextures/TB_Caltrops.png"
+#define ABILITY_TESSEN_TEXTURE "../Shurikenjutsu/2DTextures/Abilities/TB_N2_Melee.png"
+#define ABILITY_TESSEN_THROWING_TEXTURE "../Shurikenjutsu/2DTextures/Abilities/TB_N2_Q.png"
+#define ABILITY_WHIP_TEXTURE "../Shurikenjutsu/2DTextures/Abilities/TB_N2_Ranged.png"
+#define ABILITY_WHIP_SPECIAL_TEXTURE "../Shurikenjutsu/2DTextures/Abilities/TB_N2_E.png"
+#define ABILITY_CALTROP_TEXTURE "../Shurikenjutsu/2DTextures/Abilities/TB_Caltrops.png"
 
-#define ABILITY_NAGINATA_TEXTURE "../Shurikenjutsu/2DTextures/TB_N3_Melee.png"
-#define ABILITY_NAGINATA_STAB_TEXTURE "../Shurikenjutsu/2DTextures/TB_N3_Q.png"
-#define ABILITY_KUNAI_TEXTURE "../Shurikenjutsu/2DTextures/TB_N3_Ranged.png"
-#define ABILITY_KUNAI_VOLLEY_TEXTURE "../Shurikenjutsu/2DTextures/TB_N3_E.png"
-#define ABILITY_STICKY_TRAP_TEXTURE "../Shurikenjutsu/2DTextures/TB_StickyTARP.png"
+#define ABILITY_NAGINATA_TEXTURE "../Shurikenjutsu/2DTextures/Abilities/TB_N3_Melee.png"
+#define ABILITY_NAGINATA_STAB_TEXTURE "../Shurikenjutsu/2DTextures/Abilities/TB_N3_Q.png"
+#define ABILITY_KUNAI_TEXTURE "../Shurikenjutsu/2DTextures/Abilities/TB_N3_Ranged.png"
+#define ABILITY_KUNAI_VOLLEY_TEXTURE "../Shurikenjutsu/2DTextures/Abilities/TB_N3_E.png"
+#define ABILITY_STICKY_TRAP_TEXTURE "../Shurikenjutsu/2DTextures/Abilities/TB_StickyTARP.png"
 
 // Pictures
-#define NINJA_TEXTURE "../Shurikenjutsu/2DTextures/"
+#define NINJA_TEXTURE "../Shurikenjutsu/2DTextures/Ninjas/"
 
 // Cursor
-#define CURSOR_TEXTURE "../Shurikenjutsu/2DTextures/cursor.png"
+#define CURSOR_TEXTURE "../Shurikenjutsu/2DTextures/GUI/cursor.png"
 
 // Team Status Bar
-#define TEAM_STATUS_BACKGROUND "../Shurikenjutsu/2DTextures/statusBar_bg.png"
-#define TEAM_STATUS_RED_PLAYER "../Shurikenjutsu/2DTextures/statusBar_redSquare.png"
-#define TEAM_STATUS_BLUE_PLAYER "../Shurikenjutsu/2DTextures/statusBar_blueSquare.png"
-#define TEAM_STATUS_DEAD_PLAYER "../Shurikenjutsu/2DTextures/statusBar_dead.png"
-#define TEAM_STATUS_FRAME "../Shurikenjutsu/2DTextures/statusBar_frame.png"
+#define TEAM_STATUS_BACKGROUND "../Shurikenjutsu/2DTextures/GUI/statusBar_bg.png"
+#define TEAM_STATUS_RED_PLAYER "../Shurikenjutsu/2DTextures/GUI/statusBar_redSquare.png"
+#define TEAM_STATUS_BLUE_PLAYER "../Shurikenjutsu/2DTextures/GUI/statusBar_blueSquare.png"
+#define TEAM_STATUS_DEAD_PLAYER "../Shurikenjutsu/2DTextures/GUI/statusBar_dead.png"
+#define TEAM_STATUS_FRAME "../Shurikenjutsu/2DTextures/GUI/statusBar_frame.png"
 
 // DeathBoard
-#define DEATHBOARD_BACKGROUND "../Shurikenjutsu/2DTextures/abilitybarLobby.png"
-#define DEATHBOARD_NINJA1 "../Shurikenjutsu/2DTextures/DeathBoardN1.png"
-#define DEATHBOARD_NINJA2 "../Shurikenjutsu/2DTextures/DeathBoardN2.png"
-#define DEATHBOARD_NINJA3 "../Shurikenjutsu/2DTextures/DeathBoardN3.png"
+#define DEATHBOARD_BACKGROUND "../Shurikenjutsu/2DTextures/GUI/abilitybarLobby.png"
 
-#define DEATHBOARD_KILLARROW "../Shurikenjutsu/2DTextures/KillStatusDead.png"
-#define DEATHBOARD_N1MELEE "../Shurikenjutsu/2DTextures/n1melee.png"
-#define DEATHBOARD_N1RANGED "../Shurikenjutsu/2DTextures/n1ranged.png"
-#define DEATHBOARD_N1Q "../Shurikenjutsu/2DTextures/n1q.png"
-#define DEATHBOARD_N1E "../Shurikenjutsu/2DTextures/n1e.png"
-#define DEATHBOARD_N2MELEE "../Shurikenjutsu/2DTextures/n2melee.png"
-#define DEATHBOARD_N2RANGED "../Shurikenjutsu/2DTextures/n2ranged.png"
-#define DEATHBOARD_N2Q "../Shurikenjutsu/2DTextures/n2q.png"
-#define DEATHBOARD_N2E "../Shurikenjutsu/2DTextures/n2e.png"
+#define DEATHBOARD_N1MELEE "../Shurikenjutsu/2DTextures/DeathBoard/n1melee.png"
+#define DEATHBOARD_N1RANGED "../Shurikenjutsu/2DTextures/DeathBoard/n1ranged.png"
+#define DEATHBOARD_N1Q "../Shurikenjutsu/2DTextures/DeathBoard/n1q.png"
+#define DEATHBOARD_N1E "../Shurikenjutsu/2DTextures/DeathBoard/n1e.png"
 
-#define DEATHBOARD_N3MELEE "../Shurikenjutsu/2DTextures/n3melee.png"
-#define DEATHBOARD_N3RANGED "../Shurikenjutsu/2DTextures/n3ranged.png"
-#define DEATHBOARD_N3Q "../Shurikenjutsu/2DTextures/n3q.png"
-#define DEATHBOARD_N3E "../Shurikenjutsu/2DTextures/n3e.png"
+#define DEATHBOARD_N2MELEE "../Shurikenjutsu/2DTextures/DeathBoard/n2melee.png"
+#define DEATHBOARD_N2RANGED "../Shurikenjutsu/2DTextures/DeathBoard/n2ranged.png"
+#define DEATHBOARD_N2Q "../Shurikenjutsu/2DTextures/DeathBoard/n2q.png"
+#define DEATHBOARD_N2E "../Shurikenjutsu/2DTextures/DeathBoard/n2e.png"
 
-#define DEATHBOARD_CALPTROPS "../Shurikenjutsu/2DTextures/TB_Caltrops.png"
+#define DEATHBOARD_N3MELEE "../Shurikenjutsu/2DTextures/DeathBoard/n3melee.png"
+#define DEATHBOARD_N3RANGED "../Shurikenjutsu/2DTextures/DeathBoard/n3ranged.png"
+#define DEATHBOARD_N3Q "../Shurikenjutsu/2DTextures/DeathBoard/n3q.png"
+#define DEATHBOARD_N3E "../Shurikenjutsu/2DTextures/DeathBoard/n3e.png"
+
+#define DEATHBOARD_CALPTROPS "../Shurikenjutsu/2DTextures/DeathBoard/TB_Caltrops.png"
+#define DEATHBOARD_KILLARROW "../Shurikenjutsu/2DTextures/DeathBoard/Unknown.png"
 
 // Runes
 #define RUNE_SHIELD "../Shurikenjutsu/Models/shield_Shape.SSP"
 #define RUNE_INVIS "../Shurikenjutsu/Models/invisScroll_Shape.SSP"
 #define RUNE_LOTUS "../Shurikenjutsu/Models/lotus_Shape.SSP"
-// Scoreboard
-#define SCOREBOARD_BACKGROUND "../Shurikenjutsu/2DTextures/scoreboard_bg.png"
 
+// Scoreboard
+#define SCOREBOARD_BACKGROUND "../Shurikenjutsu/2DTextures/GUI/scoreboard_bg.png"
 
 #endif
