8a265536841a51077f96c8a20ab47f41b3493869
Pappskiva <alleo92@hotmail.com>
2015-03-12 15:48:39 +0100

    testting

--- Shurikenjutsu/Shurikenjutsu/CollisionManager.cpp
+++ Shurikenjutsu/Shurikenjutsu/CollisionManager.cpp
@@ -87,6 +87,7 @@ std::vector<OBB> CollisionManager::CalculateLocalPlayerCollisionWithStaticBoxes(
 		return CollisionList;
 	}
 	Sphere playerSphere = Sphere(p_playerBox.m_center, p_playerBox.m_radius);
+	playerSphere.m_position.y = 0.0f;
 	float speedXDeltaTime = p_speed * (float)GLOBAL::GetInstance().GetDeltaTime();
 	if (m_staticBoxList.size() > 0)
 	{
@@ -110,6 +111,10 @@ std::vector<OBB> CollisionManager::CalculateLocalPlayerCollisionWithStaticBoxes(
 			}
 			if (Collisions::OBBSphereCollision(box, playerSphere))
 			{
+				if (box.m_center.y > 2.0f)
+				{
+					continue;
+				}
 				CollisionList.push_back(box);
 			}
 		}
