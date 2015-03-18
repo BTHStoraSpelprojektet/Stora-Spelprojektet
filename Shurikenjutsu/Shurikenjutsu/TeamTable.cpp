e63e5abd4a61cbda0fb9ccb91530e66d0eef2638
Johan Almeflo <johan_almeflo@hotmail.com>
2015-03-18 11:51:11 +0100

    Name in lobby added

--- Shurikenjutsu/Shurikenjutsu/TeamTable.cpp
+++ Shurikenjutsu/Shurikenjutsu/TeamTable.cpp
@@ -2,6 +2,7 @@
 #include "MenuItem.h"
 #include "..\CommonLibs\TextureLibrary.h"
 #include "Globals.h"
+#include "GUIText.h"
 
 
 TeamTable::TeamTable(){}
@@ -35,6 +36,12 @@ void TeamTable::Initialize(float p_startXPos, float p_startYPos, int p_team)
 	{
 		m_toolOffset *= -1;
 	}
+
+	m_nameAlignment = DWRITE_TEXT_ALIGNMENT_LEADING;
+	if (p_team == 2)
+	{
+		m_nameAlignment = DWRITE_TEXT_ALIGNMENT_TRAILING;
+	}
 }
 void TeamTable::Shutdown()
 {
@@ -42,11 +49,15 @@ void TeamTable::Shutdown()
 	{
 		m_teamList[i].m_ninja->Shutdown();
 		m_teamList[i].m_tool->Shutdown();
+		m_teamList[i].m_name->Shutdown();
 		delete m_teamList[i].m_ninja;
 		delete m_teamList[i].m_tool;
+		delete m_teamList[i].m_name;
 		m_teamList[i].m_ninja = nullptr;
 		m_teamList[i].m_tool = nullptr;
+		m_teamList[i].m_name = nullptr;
 	}
+	m_teamList.clear();
 }
 void TeamTable::Render()
 {
@@ -54,15 +65,17 @@ void TeamTable::Render()
 	{
 		m_teamList[i].m_ninja->Render();
 		m_teamList[i].m_tool->Render();
+		m_teamList[i].m_name->Render();
 	}
 }
-void TeamTable::AddTeamMate(int p_ninja, int p_tool)
+void TeamTable::AddTeamMate(int p_ninja, int p_tool, std::string p_name)
 {
 	CharacterToolWrapper temp;
 	temp.m_tool = new MenuItem();
 	temp.m_ninja = new MenuItem();
+	temp.m_name = new GUIText();
 
-
+	// Ninja
 	if (p_ninja == 0)
 	{
 		temp.m_ninja->Initialize(m_startXPos + m_portraitOffset, m_startYPos, PORTRAITWIDTH, PORTRAITHEIGHT, TextureLibrary::GetInstance()->GetTexture((std::string)"../Shurikenjutsu/2DTextures/Ninjas/ninja1.png"));
@@ -75,6 +88,7 @@ void TeamTable::AddTeamMate(int p_ninja, int p_tool)
 	{
 		temp.m_ninja->Initialize(m_startXPos + m_portraitOffset, m_startYPos, PORTRAITWIDTH, PORTRAITHEIGHT, TextureLibrary::GetInstance()->GetTexture((std::string)"../Shurikenjutsu/2DTextures/Ninjas/ninja3.png"));
 	}
+	// Tool
 	if (p_tool == 0)
 	{
 		temp.m_tool->Initialize(m_startXPos + m_portraitOffset + m_toolOffset, m_startYPos, TOOLWIDTH, TOOLHEIGHT, TextureLibrary::GetInstance()->GetTexture((std::string)"../Shurikenjutsu/2DTextures/Abilities/TB_T_Caltrops.png"));
@@ -88,7 +102,12 @@ void TeamTable::AddTeamMate(int p_ninja, int p_tool)
 		temp.m_tool->Initialize(m_startXPos + m_portraitOffset + m_toolOffset, m_startYPos, TOOLWIDTH, TOOLHEIGHT, TextureLibrary::GetInstance()->GetTexture((std::string)"../Shurikenjutsu/2DTextures/Abilities/TB_T_StickyTARP.png"));
 	}
 
-	m_startYPos -= PORTRAITHEIGHT * 1.1f;
+	// Name
+	temp.m_name->Initialize(p_name, 40.0f, m_startXPos + m_portraitOffset * 0.75f, m_startYPos - PORTRAITHEIGHT + 15.0f, 0xFFFFFFFF);
+	temp.m_name->SetTextAlignment(m_nameAlignment);
+	temp.m_name->SetText(p_name);
+
+	m_startYPos -= PORTRAITHEIGHT * 2.0f;
 	
 	m_teamList.push_back(temp);
 }
