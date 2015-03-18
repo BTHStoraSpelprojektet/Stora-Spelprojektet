53afb558d6dcc6d6ca592f229a13efa3f3276f2a
Pappskiva <alleo92@hotmail.com>
2015-03-18 13:47:12 +0100

    humbl :dango:

--- Shurikenjutsu/CommonLibs/GameplayGlobalVariables.h
+++ Shurikenjutsu/CommonLibs/GameplayGlobalVariables.h
@@ -156,7 +156,7 @@ const float STICKY_TRAP_RANGE = 20.0f;
 const float STICKY_TRAP_DURATION = 20.0f;
 const float STICKY_TRAP_SPEED = 30.0f;
 const float STICKY_TRAP_SIZE_X = 3.0f;
-const float STICKY_TRAP_RADIUS = 2.0f;
+const float STICKY_TRAP_RADIUS = 2.3f;
 const float STICKY_TRAP_SLOW_PRECENTAGE = 0.5f;
 
 // Flashbang.
